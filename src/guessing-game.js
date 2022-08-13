5a1cf8a2750e1d84669f235e3a795072e49452a5
ErkhanDV <erhan.denis@list.ru>
2022-08-14 01:41:23 +0500

    complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      return this.mid = Math.round((this.max - this.min) / 2 + this.min);
     }
-
+  
     lower() {
-
+      return this.max = this.mid;
     }
-
+  
     greater() {
-
+      return this.min = this.mid;
     }
-}
+  }
 
 module.exports = GuessingGame;
